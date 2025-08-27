5222dd226362895b8c2d7c3ba9b0c11801669d14
Xubaear <xubaear@gmail.com>
2025-08-27 13:46:49 +0600

    implemented coin deduction

--- index.js
+++ index.js
@@ -7,9 +7,32 @@ const heartCount = document.getElementById('heart-count');
 for(let i=0; i<hearts.length; i++){
     hearts[i].addEventListener('click', function(){
         heartCounts++;
-        heartCount.textContent=heartCounts;
+        heartCount.innerText=heartCounts;
     })
 }
+
+
+// call section
+
+const coin = document.getElementById('coin-count');
+let coins = parseInt(coin.innerText);
+
+const callButtons = document.getElementsByClassName('call-btn');
+
+for(const btn of callButtons){
+    btn.addEventListener('click', function(){
+        if(coins >= 20){
+            coins = coins - 20;
+            coin.innerText = coins;
+        }else{
+            alert('You are out of credit')
+        }
+    })
+}
+
+
+
+
 
 
 
