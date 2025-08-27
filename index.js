ab4160f24b511b1a4877b923c1424bf54d79fd00
Xubaear <xubaear@gmail.com>
2025-08-27 14:08:33 +0600

    added calling alert

--- index.js
+++ index.js
@@ -21,14 +21,22 @@ const callButtons = document.getElementsByClassName('call-btn');
 
 for(const btn of callButtons){
     btn.addEventListener('click', function(){
+          const serviceName = this.getAttribute('data-service');
+        const serviceNumber = this.getAttribute('data-number');
         if(coins >= 20){
             coins = coins - 20;
             coin.innerText = coins;
+            alert(`Calling ${serviceName}.${serviceNumber}`);
         }else{
             alert('You are out of credit')
         }
     })
 }
+
+
+//real time history update
+
+
 
 
 
